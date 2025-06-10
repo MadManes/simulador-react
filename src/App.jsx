fdf855dcd0fbf1d40cb3018f60a32ceb9a7a3a4e
MadManes <laarcada@gmail.com>
2025-06-09 23:03:41 -0300

    Primer commit del simulador React

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+function App() {
+  return (
+    <div style={{ textAlign: "center", padding: "2rem" }}>
+      <h1>Simulador de Relevos</h1>
+      <img
+        src="https://relevo-proxy.onrender.com/stream"
+        alt="Stream de Relevos"
+        style={{ width: "100%", maxWidth: "720px", border: "2px solid black" }}
+      />
+    </div>
+  );
+}
+
+export default App;
+
