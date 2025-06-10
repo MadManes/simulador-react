a2ba7ad962be3009676d836725168aae93267ddb
MadManes <laarcada@gmail.com>
2025-06-09 23:27:27 -0300

    Agregada imagen del stream desde Render

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,14 @@
 function App() {
   return (
-    <div style={{ textAlign: "center", padding: "2rem" }}>
+    <div style={{ textAlign: 'center', padding: '2rem' }}>
       <h1>Simulador de Relevos</h1>
       <img
-        src="https://relevo-proxy.onrender.com/stream"
-        alt="Stream de Relevos"
-        style={{ width: "100%", maxWidth: "720px", border: "2px solid black" }}
+        src="https://relevos-proxy.onrender.com/stream"
+        alt="Relevo en vivo"
+        style={{ width: '80%', border: '2px solid black', borderRadius: '12px' }}
       />
     </div>
   );
 }
 
 export default App;
-
